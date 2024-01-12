e21f0eb75fbe653b5ca4ced14fb8d576bb21ae58
rkant6351 <153700365+rkant6351@users.noreply.github.com>
2024-01-12 13:22:39 +0530

    Add files via upload

---
+++ OrderManagementSyatem/OrderManagementSyatem/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSyatem.Exceptions
+{
+    internal class UserNotFoundException:Exception
+    {
+        public UserNotFoundException() : base("User Id is invalid")
+        {
+
+        }
+    }
+}
